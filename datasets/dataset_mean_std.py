1bac4216b1af3d1d1b9028f494be2a9e0a77d5bb
ximitiejiang <ximitiejiang@163.com>
2019-01-26 00:34:41 +0800

    add dataset mean std algorithm

---
+++ datasets/dataset_mean_std.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+"""
+参考https://github.com/kuangliu/pytorch-ssd/blob/master/utils.py
+"""
+import torch
+
+def get_mean_and_std(dataset, max_load=10000):
+    '''Compute the mean and std value of dataset.'''
+    # dataloader = torch.utils.data.DataLoader(dataset, batch_size=1, shuffle=True, num_workers=2)
+    mean = torch.zeros(3)
+    std = torch.zeros(3)
+    print('==> Computing mean and std..')
+    N = min(max_load, len(dataset))
+    for i in range(N):
+        print(i)
+        im,_,_ = dataset.load(1)
+        for j in range(3):
+            mean[j] += im[:,j,:,:].mean()
+            std[j] += im[:,j,:,:].std()
+    mean.div_(N)
+    std.div_(N)
+    return mean, std
