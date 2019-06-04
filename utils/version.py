0b99066793439352a491a56a0b4fe50718a82522
ximitiejiang <ximitiejiang@163.com>
2019-06-04 18:35:48 +0800

    update

--- utils/version.py
+++ utils/version.py
@@ -9,13 +9,14 @@ import platform
 import cv2
 import numpy as np
 import torch
+import torchvision
 
 def check_version():
     print("Python version: ", platform.python_version())
     print("opencv version: ", cv2.__version__)
     print("numpy version: ", np.__version__)
     print("pytorch version: ", torch.__version__)
-    
+    print("torchvision version: ", torchvision.__version__)    
 
 if __name__ == "__main__":
     check_version()
